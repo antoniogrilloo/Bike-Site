0f2e40c226d6d022b1bcb71ab0185a9888ec167d
a.grillo22 <a.grillo22@campus.unimib.it>
2022-12-07 18:09:39 +0100

    Aggiunto progetto vuoto

---
+++ src/main/java/it/unimib/gmp/UniBike/UniBikeApplication.java
@@ -0,0 +1,13 @@
+package it.unimib.gmp.UniBike;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UniBikeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UniBikeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/it/unimib/gmp/UniBike/UniBikeApplicationTests.java
@@ -0,0 +1,13 @@
+package it.unimib.gmp.UniBike;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UniBikeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
